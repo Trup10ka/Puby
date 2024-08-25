12059bbc159d912a80bfed456bad19318b4201b2
Trup10ka <lukys3036@gmail.com>
2024-08-25 16:39:20 +0200

    Implement registering AddMemberCommand

--- src/main/kotlin/command/AddMemberCommand.kt
+++ src/main/kotlin/command/AddMemberCommand.kt
@@ -1,12 +1,30 @@
 package me.trup10ka.puby.command
 
 import dev.kord.core.Kord
+import dev.kord.rest.builder.interaction.integer
+import dev.kord.rest.builder.interaction.user
+import dev.kord.rest.builder.interaction.string
 import me.trup10ka.puby.event.PubyEventManager
 
 object AddMemberCommand : PubyCommand
 {
     override suspend fun init(kordClient: Kord)
     {
+        kordClient.createGlobalChatInputCommand(
+            "am",
+            "Adds member to an event"
+        ) {
+            string("name", "The name of the event") { required = true }
+            user("discord_tag", "The discord tag of the member to add") { required = true }
+        }
+
+        kordClient.createGlobalChatInputCommand(
+            "am",
+            "Adds member to an event"
+        ) {
+            integer("id", "The id of the event") { required = true }
+            user("discord_tag", "The discord tag of the member to add") { required = true }
+        }
     }
 
     override suspend fun registerListener(kordClient: Kord, pubyEventManager: PubyEventManager)
