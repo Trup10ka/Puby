dc63f35365d44b3af98c0d5282722b3652df2051
Trup10ka <lukys3036@gmail.com>
2024-08-28 19:32:25 +0200

    Implement functions for embedding messages

--- src/main/kotlin/util/EmbedingUtil.kt
+++ src/main/kotlin/util/EmbedingUtil.kt
@@ -1,5 +1,37 @@
 package me.trup10ka.puby.util
 
 import dev.kord.common.Color
+import dev.kord.core.behavior.interaction.response.DeferredPublicMessageInteractionResponseBehavior
+import dev.kord.core.behavior.interaction.response.respond
+import dev.kord.rest.builder.message.EmbedBuilder
+import dev.kord.rest.builder.message.embed
+import me.trup10ka.puby.event.PubyEventMember
 
-val EMBED_COLOR = Color( 247, 168, 36)
+val EMBED_COLOR_SUCCESS = Color( 247, 168, 36)
+val EMBED_COLOR_FAIL = Color( 255, 0, 0)
+
+suspend fun DeferredPublicMessageInteractionResponseBehavior.respondEmbeddedSuccess(mention: PubyEventMember? = null, embedBuilder: EmbedBuilder.() -> Unit)
+{
+    respond {
+
+        content = mention?.toDiscordMentionFormat() ?: ""
+
+        embed {
+            embedBuilder()
+            color = EMBED_COLOR_SUCCESS
+        }
+    }
+}
+
+suspend fun DeferredPublicMessageInteractionResponseBehavior.respondEmbeddedFail(mention: PubyEventMember? = null, embedBuilder: EmbedBuilder.() -> Unit)
+{
+    respond {
+
+        content = mention?.toDiscordMentionFormat() ?: ""
+
+        embed {
+            embedBuilder()
+            color = EMBED_COLOR_FAIL
+        }
+    }
+}
