ec1be6829b1b03dfc9c9f2f85912a263fc707d4c
Trup10ka <lukys3036@gmail.com>
2024-08-31 17:56:40 +0200

    Implement new override for respondEmbbeded method

--- src/main/kotlin/util/EmbedingUtil.kt
+++ src/main/kotlin/util/EmbedingUtil.kt
@@ -1,6 +1,7 @@
 package me.trup10ka.puby.util
 
 import dev.kord.common.Color
+import dev.kord.common.entity.Snowflake
 import dev.kord.core.behavior.interaction.response.DeferredPublicMessageInteractionResponseBehavior
 import dev.kord.core.behavior.interaction.response.respond
 import dev.kord.rest.builder.message.EmbedBuilder
@@ -9,12 +10,28 @@ import me.trup10ka.puby.event.PubyEventMember
 
 val EMBED_COLOR_SUCCESS = Color( 247, 168, 36)
 val EMBED_COLOR_FAIL = Color( 255, 0, 0)
+val Snowflake.discordMentionFormat: String
+    get() = "<@$value>"
+
 
 suspend fun DeferredPublicMessageInteractionResponseBehavior.respondEmbeddedSuccess(mention: PubyEventMember? = null, embedBuilder: EmbedBuilder.() -> Unit)
 {
     respond {
 
         content = mention?.toDiscordMentionFormat() ?: ""
+
+        embed {
+            embedBuilder()
+            color = EMBED_COLOR_SUCCESS
+        }
+    }
+}
+
+suspend fun DeferredPublicMessageInteractionResponseBehavior.respondEmbeddedSuccess(mention: Snowflake, embedBuilder: EmbedBuilder.() -> Unit)
+{
+    respond {
+
+        content = mention.discordMentionFormat
 
         embed {
             embedBuilder()
