f2fa56d90f73cafad4836ef44791f7c2f9cdbb6f
Trup10ka <lukys3036@gmail.com>
2024-09-02 17:29:30 +0200

    Implement ListMembersCommand

--- src/main/kotlin/command/member/ListMembersCommand.kt
+++ src/main/kotlin/command/member/ListMembersCommand.kt
@@ -4,13 +4,16 @@ import dev.kord.core.Kord
 import dev.kord.core.behavior.interaction.response.DeferredPublicMessageInteractionResponseBehavior
 import dev.kord.core.behavior.interaction.response.respond
 import dev.kord.core.entity.interaction.ChatInputCommandInteraction
+import dev.kord.rest.builder.interaction.integer
+import me.trup10ka.puby.command.EventUtilizerCommand
 import me.trup10ka.puby.command.PubyCommand
 import me.trup10ka.puby.event.PubyEventManager
+import me.trup10ka.puby.util.respondEmbeddedSuccess
 
 class ListMembersCommand(
     commandName: String,
     commandDescription: String
-) : PubyCommand(commandName, commandDescription)
+) : EventUtilizerCommand(commandName, commandDescription)
 {
     override suspend fun init(kordClient: Kord)
     {
@@ -18,15 +21,17 @@ class ListMembersCommand(
             commandName,
             commandDescription
         ) {
-
+            integer("id", "The id of the event") { required = true }
         }
     }
 
-    override suspend fun handleCommand(
-        responseBehavior: DeferredPublicMessageInteractionResponseBehavior,
-        interaction: ChatInputCommandInteraction,
-        pubyEventManager: PubyEventManager
-    ) {
-        responseBehavior.respond { content = "List members command" }
+    override suspend fun handleCommand(responseBehavior: DeferredPublicMessageInteractionResponseBehavior, interaction: ChatInputCommandInteraction, pubyEventManager: PubyEventManager)
+    {
+        val event = getEvent(pubyEventManager, interaction.command.integers["id"]!!.toInt(), responseBehavior) ?: return
+
+        responseBehavior.respondEmbeddedSuccess {
+            title = "Members of \"${event.name}\" - ID: ${event.id}"
+            description = event.formatMembersAsDiscordEmbed()
+        }
     }
 }
