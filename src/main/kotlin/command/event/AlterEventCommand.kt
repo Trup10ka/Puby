d0abb432eeae7cfe771c1212562f3d9398ea99a5
Trup10ka <lukys3036@gmail.com>
2024-09-04 23:12:45 +0200

    Implement AlterEventCommand

--- src/main/kotlin/command/event/AlterEventCommand.kt
+++ src/main/kotlin/command/event/AlterEventCommand.kt
@@ -2,24 +2,74 @@ package me.trup10ka.puby.command.event
 
 import dev.kord.core.Kord
 import dev.kord.core.entity.interaction.ChatInputCommandInteraction
-import me.trup10ka.puby.command.PubyCommand
+import dev.kord.rest.builder.interaction.integer
+import dev.kord.rest.builder.interaction.string
+import kotlinx.datetime.LocalDate
+import kotlinx.datetime.LocalTime
+import me.trup10ka.puby.command.EventUtilizerCommand
 import me.trup10ka.puby.event.PubyEventManager
 import me.trup10ka.puby.util.DeferredResponseBehavior
+import me.trup10ka.puby.command.PubyCommandArguments.EVENT_ID
+import me.trup10ka.puby.command.PubyCommandArguments.EVENT_ALTER_PARAM
+import me.trup10ka.puby.command.PubyCommandArguments.EVENT_PARAM_NEW_VALUE
+import me.trup10ka.puby.event.PubyEvent
+import me.trup10ka.puby.util.respondEmbeddedFail
+import me.trup10ka.puby.util.respondEmbeddedSuccess
 
 class AlterEventCommand(
     commandName: String,
     commandDescription: String
-) : PubyCommand(commandName, commandDescription)
+) : EventUtilizerCommand(commandName, commandDescription)
 {
     override suspend fun init(kordClient: Kord)
     {
+        kordClient.createGlobalChatInputCommand(
+            commandName,
+            commandDescription
+        ) {
+            integer(EVENT_ID.argName, EVENT_ID.description) { required = true }
+            string(EVENT_ALTER_PARAM.argName, EVENT_ALTER_PARAM.description) {
+                required = true
+                choice("N", "Name")
+                choice("DE", "Description")
+                choice("P", "Place")
+                choice("DA", "Date")
+                choice("T", "Time")
+            }
+            string(EVENT_PARAM_NEW_VALUE.argName,EVENT_PARAM_NEW_VALUE.description) { required = true }
+        }
     }
 
-    override suspend fun handleCommand(
-        responseBehavior: DeferredResponseBehavior,
-        interaction: ChatInputCommandInteraction,
-        pubyEventManager: PubyEventManager
-    ) {
+    override suspend fun handleCommand(responseBehavior: DeferredResponseBehavior, interaction: ChatInputCommandInteraction, pubyEventManager: PubyEventManager)
+    {
+        val event = getEvent(pubyEventManager, interaction.command.integers["id"]!!.toInt(), responseBehavior) ?: return
+
+        if (updateEventParameter(event, interaction.command.strings))
+            responseBehavior.respondEmbeddedSuccess {
+                title = "Event \"${event.name}\" - ID: ${event.id} updated"
+                description = event.toFancyString()
+            }
+        else
+            responseBehavior.respondEmbeddedFail {
+                title = "Invalid event parameter"
+                description = "The event parameter provided is invalid"
+            }
     }
 
+    private fun updateEventParameter(event: PubyEvent, commandStrings: Map<String, String>): Boolean
+    {
+        val eventParam = commandStrings["event_param"]!!
+        val newValue = commandStrings["new_value"]!!
+
+        when(eventParam)
+        {
+            "N" -> event.name = newValue
+            "DE" -> event.description = newValue
+            "P" -> event.place = newValue
+            "DA" -> event.date = LocalDate.parse(newValue)
+            "T" -> event.time = LocalTime.parse(newValue)
+            else -> return false
+        }
+        return true
+    }
 }
