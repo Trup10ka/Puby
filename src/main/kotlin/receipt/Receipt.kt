5b03f4a4ea42cde73b75b56437e0880e95992826
Trup10ka <lukys3036@gmail.com>
2026-07-01 14:05:19 +0200

    Add receiptId property to Receipt class

--- src/main/kotlin/receipt/Receipt.kt
+++ src/main/kotlin/receipt/Receipt.kt
@@ -3,9 +3,11 @@ package me.trup10ka.puby.receipt
 import me.trup10ka.puby.data.PubyEventMember
 
 class Receipt(
+    val receiptId: Int,
     val payers: MutableList<PubyEventMember> = mutableListOf(),
     val payersAccounts: MutableMap<PubyEventMember, String> = mutableMapOf(),
     val itemsPurchased: MutableList<ReceiptItem> = mutableListOf()
 )
 {
+
 }
