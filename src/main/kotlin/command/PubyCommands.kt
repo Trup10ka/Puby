a027b8782bf4038403ca884d2f67078364503766
Trup10ka <lukys3036@gmail.com>
2026-07-01 14:04:16 +0200

    Add SHOW_EVENT command to PubyCommands

--- src/main/kotlin/command/PubyCommands.kt
+++ src/main/kotlin/command/PubyCommands.kt
@@ -5,8 +5,8 @@ enum class PubyCommands(val abbreviation: String, val description: String)
     CREATE_EVENT("ce", "Create a new event"),
     DELETE_EVENT("de", "Delete an event"),
     ALTER_EVENT("ae", "Alter an event"),
+    SHOW_EVENT("se", "Show an event details"),
     ADD_MEMBER("am", "Add a member to an event"),
     REMOVE_MEMBER("rm", "Remove a member from an event"),
-    LIST_MEMBERS("lm", "List members of an event"),
-    SHOW_EVENT("se", "Show an event details")
+    LIST_MEMBERS("lm", "List members of an event")
 }
